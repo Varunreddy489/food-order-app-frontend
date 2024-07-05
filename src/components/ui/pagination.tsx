eb3957a1f59bc7806a8004e8bd9913b681079c16
Varunreddy489 <varunsannapureddy@gmail.com>
2024-07-05 10:08:40 +0530

    first commit 	modified:   src/components/ui/badge.tsx 	modified:   src/components/ui/card.tsx 	modified:   src/components/ui/checkbox.tsx 	modified:   src/components/ui/dialog.tsx 	modified:   src/components/ui/pagination.tsx 	modified:   src/components/ui/progress.tsx 	modified:   src/components/ui/select.tsx 	modified:   src/components/ui/tabs.tsx

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -5,8 +5,9 @@ import {
   DotsHorizontalIcon,
 } from "@radix-ui/react-icons"
 
-import { cn } from " "
-import { ButtonProps, buttonVariants } from "@/components/ui/button"
+import { cn } from "../../lib/utils"
+import { ButtonProps, buttonVariants } from "./button"
+
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
